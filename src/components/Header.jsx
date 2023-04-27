e2cb7853b22190921357dc9fbe520ab6ea1db92b
Carolliny Ribeiro <carollinyribeiro@pop-os.localdomain>
2023-04-27 11:44:15 -0300

    feat: implantig logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,13 @@ function Header(){
             <div id="titulo">
                 JordanShoes
             </div>
+            <div id="logo">
+                <h1>A melhor loja de Jordan</h1> 
+                <p>O tênis Jordan é fruto de uma velha e forte parceria <br/> entre a Nike e o jogador Michael Jordan.</p>
+            </div>
+            <div id="teste">
+
+            </div>
         </header>
     )
 }
